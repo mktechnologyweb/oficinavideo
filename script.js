98008d50a0331c21b22cc536ab96ed6c8e44b649
mktechnologyweb <mktechnologyweb@gmail.com>
2023-02-03 11:49:23 -0300

    oficina

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 800) {
+          $(".fixed-top").css("background" , "#1B1B1B");
+        }
+  
+        else{
+            $(".fixed-top").css("background" , "transparent");  	
+        }
+    })
+  })
+
+
+  	const toggleSwitch =
+	document.querySelector('.theme-slider input[type="checkbox"]');
+
+/* Function to change theme */
+function switchTheme(e) {
+
+	/* Once checkbox is checked default theme change to dark */
+	if (e.target.checked) {
+		document.documentElement.setAttribute('theme', 'dark');
+	}
+
+	/* While page in dark mode and checkbox is
+	checked then theme back to change light*/
+	else {
+		document.documentElement.setAttribute('theme', 'light');
+	}
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
